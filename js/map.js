9b2ded5ccbb182ea72f8b80887b54cc42e5f88c1
icarus4 <icarus4.chu@gmail.com>
2014-03-22 15:25:42 +0800

    tmp

--- js/map.js
+++ js/map.js
@@ -1,15 +1,39 @@
 //function initialize() {
 $( document ).ready(function() {
+
+  // $(".map-area #map-canvas").fancybox({
+  //   'hideOnContentClick': true,
+  //   'overlayColor'    : '#6b0202',
+  //   'overlayOpacity'  : 0.8
+  //   });
+
+
+  var map = "";
+
+  $('#place').fancybox({
+    'hideOnContentClick': false, // so you can handle the map
+      'overlayColor'      : '#ccffee',
+      'overlayOpacity'    : 0.2,
+      'autoDimensions': true, // the selector #mapcontainer HAS css width and height
+      'beforeShow': function(){
+        google.maps.event.trigger(map, "resize");
+    },
+    'onCleanup': function() {
+       var myContent = this.href;
+       $(myContent).unwrap();
+      } // fixes inline bug
+  });
+
   var myLatlng = new google.maps.LatLng(25.003882,121.504922);
-  var mapOptions = {
-    zoom: 10,
-    center: myLatlng
-  };
+    var mapOptions = {
+      zoom: 10,
+      center: myLatlng
+    };
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    //var map = new google.maps.Map(document.getElementById('map-canvas'),
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
-
-});
+  });
 
 //initialize();
